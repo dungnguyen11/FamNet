34543476f58cf4bc3b71f7cf470d308b80e6ba09
tung lam <s3598768@rmit.edu.vn>
2017-12-04 01:14:38 +0700

    New Task UI /Personal Task Board UI

--- app/src/main/java/com/famnet/famnet/PersonalTaskBoardActivity.java
+++ app/src/main/java/com/famnet/famnet/PersonalTaskBoardActivity.java
@@ -1,7 +1,9 @@
 package com.famnet.famnet;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class PersonalTaskBoardActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class PersonalTaskBoardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_task_board);
+    }
+    ///CLICK ON ADD NEW TASK BUTTON
+    public void onClickAddNewTask(View view) {
+        Intent intent = new Intent(this, NewTaskActivity.class);
+        startActivity(intent);
     }
 }
