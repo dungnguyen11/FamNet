d0a929293fc2b7de38641a82df717f6a65cfaa2a
Dung Nguyen <nguyentridung165@gmail.com>
2017-12-07 02:45:01 +0700

    add test Activity

--- app/src/main/java/com/famnet/famnet/TasksActivity.java
+++ app/src/main/java/com/famnet/famnet/TasksActivity.java
@@ -2,11 +2,8 @@ package com.famnet.famnet;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 public class TasksActivity extends AppCompatActivity {
 
@@ -25,30 +22,30 @@ public class TasksActivity extends AppCompatActivity {
 
     }
 
-    private void createTaskLayout(LinearLayout container){
-        //Testing
-        testButton = findViewById(R.id.test_button);
-        testButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.d(TAG, "clicked");
-                LinearLayout horizontalContainer = new LinearLayout(TasksActivity.this, null);
-                horizontalContainer.setOrientation(LinearLayout.HORIZONTAL);
-//                horizontalContainer.set
-
-                Button takeButton = new Button(TasksActivity.this, null);
-                horizontalContainer.addView(takeButton);
-
-                LinearLayout verticalContainer = new LinearLayout(TasksActivity.this, null);
-                horizontalContainer.addView(verticalContainer);
-                TextView taskNameTextView = new TextView(TasksActivity.this, null);
-                TextView taskRewardTextView = new TextView(TasksActivity.this, null);
-                TextView taskDeadlineTextView = new TextView(TasksActivity.this, null);
-                verticalContainer.addView(taskNameTextView);
-                verticalContainer.addView(taskRewardTextView);
-                verticalContainer.addView(taskDeadlineTextView);
-
-            }
-        });
-    }
+//    private void createTaskLayout(LinearLayout container){
+//        //Testing
+//        testButton = findViewById(R.id.test_button);
+//        testButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Log.d(TAG, "clicked");
+//                LinearLayout horizontalContainer = new LinearLayout(TasksActivity.this, null);
+//                horizontalContainer.setOrientation(LinearLayout.HORIZONTAL);
+////                horizontalContainer.set
+//
+//                Button takeButton = new Button(TasksActivity.this, null);
+//                horizontalContainer.addView(takeButton);
+//
+//                LinearLayout verticalContainer = new LinearLayout(TasksActivity.this, null);
+//                horizontalContainer.addView(verticalContainer);
+//                TextView taskNameTextView = new TextView(TasksActivity.this, null);
+//                TextView taskRewardTextView = new TextView(TasksActivity.this, null);
+//                TextView taskDeadlineTextView = new TextView(TasksActivity.this, null);
+//                verticalContainer.addView(taskNameTextView);
+//                verticalContainer.addView(taskRewardTextView);
+//                verticalContainer.addView(taskDeadlineTextView);
+//
+//            }
+//        });
+//    }
 }
--- app/src/main/java/com/famnet/famnet/TestActivity.java
+++ app/src/main/java/com/famnet/famnet/TestActivity.java
@@ -1,13 +1,66 @@
 package com.famnet.famnet;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TestActivity extends AppCompatActivity {
 
+    private Button accountButton;
+    private Button chatButton;
+    private Button newTaskButton;
+    private Button personalTaskButton;
+    private Button tasksButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+        //init view
+        accountButton = findViewById(R.id.account_activity_button);
+        accountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(TestActivity.this, AccountActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        chatButton = findViewById(R.id.chat_activity_button);
+        chatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(TestActivity.this, ChatActivity.class);
+                startActivity(intent);
+            }
+        });
+        newTaskButton = findViewById(R.id.new_task_activity_button);
+        newTaskButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(TestActivity.this, NewTaskActivity.class);
+                startActivity(intent);
+            }
+        });
+        personalTaskButton = findViewById(R.id.personal_task_activity_button);
+        personalTaskButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(TestActivity.this, PersonalTasksActivity.class);
+                startActivity(intent);
+            }
+        });
+        tasksButton = findViewById(R.id.tasks_activity_button);
+        tasksButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(TestActivity.this, TasksActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 }
