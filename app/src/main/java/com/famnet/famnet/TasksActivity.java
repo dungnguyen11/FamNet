e5351f4999e19ad6e21a2b30c64a729109296032
tung lam <s3598768@rmit.edu.vn>
2017-12-26 21:12:10 +0700

    Fix notification for new task added to taskboard and notiication new message. Fix calendar bug for deadline in create new task

--- app/src/main/java/com/famnet/famnet/TasksActivity.java
+++ app/src/main/java/com/famnet/famnet/TasksActivity.java
@@ -1,5 +1,8 @@
 package com.famnet.famnet;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -126,6 +129,20 @@ public class TasksActivity extends AppCompatActivity {
                 Log.d("mTaskBoard value", mTaskBoard.get(0).getId());
 
                 updateUI(mTaskBoard);
+
+                Intent intent = new Intent();
+                PendingIntent pendingIntent = PendingIntent.getActivity(TasksActivity.this,0, intent, 0);
+                Notification noti = new Notification.Builder(TasksActivity.this)
+                        .setTicker("Famnet New Task")
+                        .setContentTitle("Famnet - New Task In Family TaskBoard")
+                        .setContentText("Someone in your family just created a new task. Check it now !")
+                        .setSmallIcon(R.drawable.notification)
+                        .setContentIntent(pendingIntent).getNotification();
+
+
+                noti.flags = Notification.FLAG_AUTO_CANCEL;
+                NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                notificationManager.notify(0,noti);
             }
 
             @Override
@@ -199,8 +216,8 @@ public class TasksActivity extends AppCompatActivity {
         public void bind(Task task) {
             mTask = task;
             mTaskNameTextView.setText(mTask.getName());
-            mTaskRewardTextView.setText(mTask.getReward());
-            mTaskDeadlineTextView.setText(mTask.getDeadline());
+            mTaskRewardTextView.setText("Reward: "+ mTask.getReward());
+            mTaskDeadlineTextView.setText("Deadline: " + mTask.getDeadline());
         }
 
         //TODO:implment onClick()
